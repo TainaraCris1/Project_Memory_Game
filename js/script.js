b1af5f6b9ecd1f42b21a5d2eeb6bde7188a5b0fb
TainaraCris1 <tainaracris1506@gmail.com>
2022-05-25 10:59:25 +0200

    basico

--- js/script.js
+++ js/script.js
@@ -11,9 +11,11 @@ function flipCard(){
 
     this.classList.add("flip");
 
+    // if(!hasFlippedCard){time()}
     if(!hasFlippedCard){
         //first click
         hasFlippedCard = true;
+        time();
         firstCard = this;
 
         return;
@@ -51,12 +53,14 @@ function unflipCards(){
 
     //not a match
     setTimeout(() =>{
+    firstCard.classList.add("shake")
+    secondCard.classList.add("shake")
     firstCard.classList.remove("flip");
     secondCard.classList.remove("flip");
 
     resetBoard();
 
-    }, 1500);   
+    }, 1000);   
 }
 
 function resetBoard(){
@@ -71,7 +75,41 @@ function resetBoard(){
         let randomPos = Math.floor(Math.random() * 12);
         card.style.order = randomPos;
     })
-})(); // extra parenteses Immediately Invoked Function Expression
+})(); //sera a primeira funcao a realizar de
 
 
 cards.forEach(card => card.addEventListener("click", flipCard));
+
+
+// for(let i = 0; i < cards.length; i++){
+//     cards[i].addEventListener("click", ()=>{
+//         if(!firstCard){time()}
+//         firstCard = true;
+//         if(cards[i].state == 'unclicked'){
+//             cards[i].style.transform = 'rotateY(180deg)'
+//             cards[i].state ='clicked'
+//             counter++
+//             cardPair.push(cards[i])
+//             check()
+//         }
+//     })
+// }
+
+function time{
+    let secs = 0;
+    let mins = 0;
+    let SS;
+    let MM;
+    setInterval(()=>{
+        secs++
+        if(secs===60){secs=0; mins++}
+
+        secs<10?SS="0${secs}':SS='${secs}"
+        secs<10?MM='0${mins}':SS='${mins}'
+
+        document.querySelector('#time').innerHTML = '${MM}:${SS}'
+    }, 1000);
+}
+
+
+// function time()
